0a063c7de8eca96d36f8d91fc2db30ed4b409e34
Marianiki Zafeiraki <marnikzaf@gmail.com>
2025-04-27 21:35:55 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -130,8 +130,7 @@ session_name = st.sidebar.selectbox(
     options=["(new session)"] + saved_sessions,
     index=session_default_index,
     format_func=lambda x: "Create new session" if x == "(new session)" else x,
-    key="session_select"
-)
+    key=f"session_select_{len(saved_sessions)}"
 
 # --- Create new session ---
 if session_name == "(new session)":
